30f54a441b2db5d94ddce99f737f6c23e5bf8627
TSHEPHOKHAME\theo1 <tshepho@pcgsoftware.co>
2025-02-10 21:25:02 +0200

    first commit

---
+++ lib/ui/common/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const Color kcPrimaryColor = Color(0xFF9600FF);
+const Color kcPrimaryColorDark = Color(0xFF300151);
+const Color kcBlack = Color(0xFF000000);
+const Color kcDarkGreyColor = Color(0xFF1A1B1E);
+const Color kcMediumGrey = Color(0xFF474A54);
+const Color kcLightGrey = Color.fromARGB(255, 187, 187, 187);
+const Color kcVeryLightGrey = Color(0xFFE3E3E3);
+const Color kcWhite = Color(0xFFFFFFFF);
+const Color kcBackgroundColor = kcDarkGreyColor;
+
+const Color kcred = Color(0xFFD93A0B);
+const Color kcgreen = Color(0xFF1E8F3A);
+
+const Color kcrose50 = Color(0xFFFAEDE6);
+const Color kcrose100 = Color(0xFFF2E0D9);
+const Color kcrose300 = Color(0xFFCCB8B5);
+const Color kcrose400 = Color(0xFF998F99);
+const Color kcrose500 = Color(0xFF705F70);
+const Color kcrose900 = Color(0xFF1A0F0F);
---
+++ lib/ui/common/app_constants.dart
@@ -0,0 +1,9 @@
+// /// The max width the content can ever take up on the screen
+// const double kdDesktopMaxContentWidth = 1150;
+
+// // The max height the homeview will take up
+// const double kdDesktopMaxContentHeight = 750;
+import 'package:flutter/material.dart';
+
+const double kdDesktopScreenWidth = 1440;
+const double kdMobileScreenwidth = 375;
